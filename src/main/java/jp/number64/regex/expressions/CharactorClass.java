6a31977c9a7f4198399ac9e49610c202c0e1b474
TODOROKI Daisuke <number64tech@gmail.com>
2016-01-11 12:33:01 +0900

    Updated CharactorClass (sample-pattern & test)

--- src/main/java/jp/number64/regex/expressions/CharactorClass.java
+++ src/main/java/jp/number64/regex/expressions/CharactorClass.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 
 /**
  * Character classes  <br>
+ * [] : Box brackets, Square brackets.  <br>
  *  <br>
  * [abc] a, b, or c (simple class)  <br>
  * [^abc] Any character except a, b, or c (negation)  <br>
@@ -15,24 +16,28 @@ import org.slf4j.LoggerFactory;
  * [a-z&&[def]] d, e, or f (intersection) (Google transrate: Set consists of all the elements that belong to also to A B)  <br>
  * [a-z&&[^bc]] a through z, except for b and c: ad-z  <br>
  * [a-z&&[^m-p]] a through z, and not m through p: a-lq-z  <br>
+ *  <br>
+ * memo : [abc[def]] = [[abc][def]] = [[ab][cd]][ef] <br>
  */
 public class CharactorClass {
     private static final Logger LOGGER = LoggerFactory.getLogger(CharactorClass.class);
 
-    /** [abc] a, b, or c (simple class)  <br> */
+    /** [abc] a, b, or c (simple class)  */
     public static final String EXP_SIMPLE_CHARS = "[nN][uU]MBER64";
-    /** [^abc] Any character except a, b, or c (negation)  <br> */
+    /** [^abc] Any character except a, b, or c (negation)  */
     public static final String EXP_NEGATION_CHARS = "[^nN][^uU]MBER64";
-    /** [a-zA-Z] a through z or A through Z, inclusive (range)  <br> */
+    /** [a-zA-Z] a through z or A through Z, inclusive (range)  */
     public static final String EXP_RANGE_CHARS = "[a-z][A-Z]MBER[0-9][4-6]";
     /** [a-d[m-p]] a through d, or m through p: a-dm-p */
-    public static final String EXP_RANGE_OR_RANGE = "NUMBER64";
+    public static final String EXP_RANGE_OR_RANGE = "[[n-z][A-N]]UMBER[[0-1][3-4][679]]4";
     /** [a-z&&[def]] d, e, or f (intersection) */
-    public static final String EXP_RANGE_AND_RANGE = "NUMBER64";
+    public static final String RANGE_A_END   = "q";
+    public static final String RANGE_B_START = "m";
+    public static final String EXP_RANGE_AND_RANGE = "[a-" + RANGE_A_END + "&&[" + RANGE_B_START + "-n]]UMBER64";
     /** [a-z&&[^bc]] a through z, except for b and c: ad-z */
-    public static final String EXP_RANGE_EXCEPTING_CHARS = "NUMBER64";
+    public static final String EXP_RANGE_EXCEPTING_CHARS = "[a-z&&[^abcdefghijklmopqrstuvwxyz]]UMBER64";
     /** [a-z&&[^m-p]] a through z, and not m through p: a-lq-z */
-    public static final String EXP_RANGE_EXCEPTING_RANGE = "NUMBER64";
+    public static final String EXP_RANGE_EXCEPTING_RANGE = "[[[A-E]&&[^B-D]][F-N]]UMBER64";
 
     // -----------------------------------------------------------------------------------------------
 
--- src/test/java/jp/number64/regex/expressions/CharactorClassTest.java
+++ src/test/java/jp/number64/regex/expressions/CharactorClassTest.java
@@ -3,7 +3,11 @@ package jp.number64.regex.expressions;
 import static jp.number64.regex.expressions.CharactorClass.*;
 import static org.junit.Assert.*;
 import jp.number64.regex.expressions.CharactorClassTest.NegationCharsTest;
+import jp.number64.regex.expressions.CharactorClassTest.RangeAndRangeTest;
 import jp.number64.regex.expressions.CharactorClassTest.RangeCharsTest;
+import jp.number64.regex.expressions.CharactorClassTest.RangeExceptingCharsTest;
+import jp.number64.regex.expressions.CharactorClassTest.RangeExceptingRangeTest;
+import jp.number64.regex.expressions.CharactorClassTest.RangeOrRangeTest;
 import jp.number64.regex.expressions.CharactorClassTest.SimpleCharsTest;
 
 import org.junit.Before;
@@ -19,7 +23,11 @@ import org.slf4j.LoggerFactory;
 @SuiteClasses({
     SimpleCharsTest.class,
     NegationCharsTest.class,
-    RangeCharsTest.class
+    RangeCharsTest.class,
+    RangeOrRangeTest.class,
+    RangeAndRangeTest.class,
+    RangeExceptingCharsTest.class,
+    RangeExceptingRangeTest.class
 })
 public class CharactorClassTest {
     private static final Logger LOGGER = LoggerFactory.getLogger(CharactorClassTest.class);
@@ -78,6 +86,82 @@ public class CharactorClassTest {
             assertFalse(target.findRangeChars("NuMBER64"));
             assertFalse(target.findRangeChars("nuMBER64"));
             assertFalse(target.findRangeChars("nUMBER63"));
+        }
+    }
+
+    /**  */
+    public static final class RangeOrRangeTest extends TestBase {
+        @BeforeClass
+        public static void doBeforeClass() {
+            LOGGER.debug("EXP_RANGE_OR_RANGE: pattern=\"{}\"", EXP_RANGE_OR_RANGE);
+        }
+        @Test
+        public void caseTrue() {
+            assertTrue(target.findRangeOrRange("nUMBER04"));
+            assertTrue(target.findRangeOrRange("NUMBER34"));
+            assertTrue(target.findRangeOrRange("NUMBER64"));
+        }
+        @Test
+        public void caseFalse() {
+            assertFalse(target.findRangeOrRange("mUMBER04"));
+            assertFalse(target.findRangeOrRange("OUMBER04"));
+            assertFalse(target.findRangeOrRange("NUMBER24"));
+            assertFalse(target.findRangeOrRange("NUMBER54"));
+            assertFalse(target.findRangeOrRange("NUMBER84"));
+        }
+    }
+
+    /**  */
+    public static final class RangeAndRangeTest extends TestBase {
+        @BeforeClass
+        public static void doBeforeClass() {
+            LOGGER.debug("EXP_RANGE_AND_RANGE: pattern=\"{}\"", EXP_RANGE_AND_RANGE);
+        }
+        @Test
+        public void caseTrue() {
+            assertTrue(target.findRangeAndRange("mUMBER64"));
+            assertTrue(target.findRangeAndRange("nUMBER64"));
+        }
+        @Test
+        public void caseFalse() {
+            assertFalse(target.findRangeAndRange("aUMBER64"));
+            assertFalse(target.findRangeAndRange("oUMBER64"));
+        }
+    }
+
+    /**  */
+    public static final class RangeExceptingCharsTest extends TestBase {
+        @BeforeClass
+        public static void doBeforeClass() {
+            LOGGER.debug("EXP_RANGE_EXCEPTING_CHARS: pattern=\"{}\"", EXP_RANGE_EXCEPTING_CHARS);
+        }
+        @Test
+        public void caseTrue() {
+            assertTrue(target.findRangeExceptingChars("nUMBER64"));
+        }
+        @Test
+        public void caseFalse() {
+            assertFalse(target.findRangeExceptingChars("mUMBER64"));
+            assertFalse(target.findRangeExceptingChars("oUMBER64"));
+        }
+    }
+
+    /**  */
+    public static final class RangeExceptingRangeTest extends TestBase {
+        @BeforeClass
+        public static void doBeforeClass() {
+            LOGGER.debug("EXP_RANGE_EXCEPTING_RANGE: pattern=\"{}\"", EXP_RANGE_EXCEPTING_RANGE);
+        }
+        @Test
+        public void caseTrue() {
+            assertTrue(target.findRangeExceptingRange("AUMBER64"));
+            assertTrue(target.findRangeExceptingRange("EUMBER64"));
+            assertTrue(target.findRangeExceptingRange("NUMBER64"));
+        }
+        @Test
+        public void caseFalse() {
+            assertFalse(target.findRangeExceptingRange("BUMBER64"));
+            assertFalse(target.findRangeExceptingRange("OUMBER64"));
         }
     }
 
