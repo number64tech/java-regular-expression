4c48b9341ca5fa844130e112179320c14d1eb0da
TODOROKI Daisuke <number64tech@gmail.com>
2016-01-12 14:37:49 +0900

    Implement sample-methods and dummy test-methods

---
+++ src/main/java/jp/number64/regex/expressions/PredefinedCharacterClasses.java
@@ -0,0 +1,117 @@
+package jp.number64.regex.expressions;
+
+import java.util.regex.Pattern;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Predefined character classes  <br>
+ * <br>
+ * . Any character (may or may not match line terminators)  <br>
+ * \d A digit: [0-9]  <br>
+ * \D A non-digit: [^0-9]  <br>
+ * \h A horizontal whitespace character: [ \t\xA0\u1680\u180e\u2000-\u200a\u202f\u205f\u3000]  <br>
+ * \H A non-horizontal whitespace character: [^\h]  <br>
+ * \s A whitespace character: [ \t\n\x0B\f\r]  <br>
+ * \S A non-whitespace character: [^\s]  <br>
+ * \v A vertical whitespace character: [\n\x0B\f\r\x85\u2028\u2029]  <br>
+ * \V A non-vertical whitespace character: [^\v]  <br>
+ * \w A word character: [a-zA-Z_0-9]  <br>
+ * \W A non-word character: [^\w]  <br>
+ */
+public class PredefinedCharacterClasses {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PredefinedCharacterClasses.class);
+
+    /** . Any character (may or may not match line terminators) */
+    public static final String EXP_ANY_CHARACTER = "";
+    /** \d A digit: [0-9] */
+    public static final String EXP_A_DIGIT = "";
+    /** \D A non-digit: [^0-9] */
+    public static final String EXP_A_NON_DIGIT = "";
+    /** \h A horizontal whitespace character: [ \t\xA0\u1680\u180e\u2000-\u200a\u202f\u205f\u3000] */
+    public static final String EXP_HORIZONTAL_WHITE_SPACE = "";
+    /** \H A non-horizontal whitespace character: [^\h] */
+    public static final String EXP_NON_HORIZONTAL_WHITE_SPACE = "";
+    /** \s A whitespace character: [ \t\n\x0B\f\r] */
+    public static final String EXP_WHITE_SPACE = "";
+    /** \S A non-whitespace character: [^\s] */
+    public static final String EXP_NON_WHITE_SPACE = "";
+    /** \v A vertical whitespace character: [\n\x0B\f\r\x85\u2028\u2029] */
+    public static final String EXP_VERTICAL_WHITE_SPACE = "";
+    /** \V A non-vertical whitespace character: [^\v] */
+    public static final String EXP_NON_VERTICAL_WHITE_SPACE = "";
+    /** \w A word character: [a-zA-Z_0-9] */
+    public static final String EXP_A_WORD_CHARACTER = "";
+    /** \W A non-word character: [^\w] */
+    public static final String EXP_A_NON_WORD_CHARACTER = "";
+
+    // -----------------------------------------------------------------------------------------------
+
+    private static final Pattern PATTERN_ANY_CHARACTER = Pattern.compile(EXP_ANY_CHARACTER);
+    private static final Pattern PATTERN_A_DIGIT = Pattern.compile(EXP_A_DIGIT);
+    private static final Pattern PATTERN_A_NON_DIGIT = Pattern.compile(EXP_A_NON_DIGIT);
+    private static final Pattern PATTERN_HORIZONTAL_WHITE_SPACE = Pattern.compile(EXP_HORIZONTAL_WHITE_SPACE);
+    private static final Pattern PATTERN_NON_HORIZONTAL_WHITE_SPACE = Pattern.compile(EXP_NON_HORIZONTAL_WHITE_SPACE);
+    private static final Pattern PATTERN_WHITE_SPACE = Pattern.compile(EXP_WHITE_SPACE);
+    private static final Pattern PATTERN_NON_WHITE_SPACE = Pattern.compile(EXP_NON_WHITE_SPACE);
+    private static final Pattern PATTERN_VERTICAL_WHITE_SPACE = Pattern.compile(EXP_VERTICAL_WHITE_SPACE);
+    private static final Pattern PATTERN_NON_VERTICAL_WHITE_SPACE = Pattern.compile(EXP_NON_VERTICAL_WHITE_SPACE);
+    private static final Pattern PATTERN_A_WORD_CHARACTER = Pattern.compile(EXP_A_WORD_CHARACTER);
+    private static final Pattern PATTERN_A_NON_WORD_CHARACTER = Pattern.compile(EXP_A_NON_WORD_CHARACTER);
+
+    public boolean findAnyCharacter(String text) {
+        LOGGER.debug("text:{}", text);
+        return PATTERN_ANY_CHARACTER.matcher(text).find();
+    }
+
+    public boolean findDigit(String text) {
+        LOGGER.debug("text:{}", text);
+        return PATTERN_A_DIGIT.matcher(text).find();
+    }
+
+    public boolean findNonDigit(String text) {
+        LOGGER.debug("text:{}", text);
+        return PATTERN_A_NON_DIGIT.matcher(text).find();
+    }
+
+    public boolean findHorizontalWhiteSpace(String text) {
+        LOGGER.debug("text:{}", text);
+        return PATTERN_HORIZONTAL_WHITE_SPACE.matcher(text).find();
+    }
+
+    public boolean findNonHorizontalWhiteSpace(String text) {
+        LOGGER.debug("text:{}", text);
+        return PATTERN_NON_HORIZONTAL_WHITE_SPACE.matcher(text).find();
+    }
+
+    public boolean findWhiteSpace(String text) {
+        LOGGER.debug("text:{}", text);
+        return PATTERN_WHITE_SPACE.matcher(text).find();
+    }
+
+    public boolean findNonWhiteSpace(String text) {
+        LOGGER.debug("text:{}", text);
+        return PATTERN_NON_WHITE_SPACE.matcher(text).find();
+    }
+
+    public boolean findVerticalWhiteSpace(String text) {
+        LOGGER.debug("text:{}", text);
+        return PATTERN_VERTICAL_WHITE_SPACE.matcher(text).find();
+    }
+
+    public boolean findNonVerticalWhiteSpace(String text) {
+        LOGGER.debug("text:{}", text);
+        return PATTERN_NON_VERTICAL_WHITE_SPACE.matcher(text).find();
+    }
+
+    public boolean findWordCharacter(String text) {
+        LOGGER.debug("text:{}", text);
+        return PATTERN_A_WORD_CHARACTER.matcher(text).find();
+    }
+
+    public boolean findNonWordCharacter(String text) {
+        LOGGER.debug("text:{}", text);
+        return PATTERN_A_NON_WORD_CHARACTER.matcher(text).find();
+    }
+}
---
+++ src/test/java/jp/number64/regex/expressions/RunningPredefinedCharacterClassTest.java
@@ -0,0 +1,251 @@
+package jp.number64.regex.expressions;
+
+import static jp.number64.regex.expressions.PredefinedCharacterClasses.*;
+import static org.junit.Assert.*;
+import jp.number64.regex.expressions.RunningPredefinedCharacterClassTest.PredefAnyCharacterTest;
+import jp.number64.regex.expressions.RunningPredefinedCharacterClassTest.PredefDigitTest;
+import jp.number64.regex.expressions.RunningPredefinedCharacterClassTest.PredefHorizontalWhiteSpaceTest;
+import jp.number64.regex.expressions.RunningPredefinedCharacterClassTest.PredefNonDigitTest;
+import jp.number64.regex.expressions.RunningPredefinedCharacterClassTest.PredefNonHorizontalWhiteSpaceTest;
+import jp.number64.regex.expressions.RunningPredefinedCharacterClassTest.PredefNonVerticalWhiteSpaceTest;
+import jp.number64.regex.expressions.RunningPredefinedCharacterClassTest.PredefNonWhiteSpaceTest;
+import jp.number64.regex.expressions.RunningPredefinedCharacterClassTest.PredefNonWordCharacterTest;
+import jp.number64.regex.expressions.RunningPredefinedCharacterClassTest.PredefVerticalWhiteSpaceTest;
+import jp.number64.regex.expressions.RunningPredefinedCharacterClassTest.PredefWhiteSpaceTest;
+import jp.number64.regex.expressions.RunningPredefinedCharacterClassTest.PredefWordCharacterTest;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+    PredefAnyCharacterTest.class,
+    PredefDigitTest.class,
+    PredefNonDigitTest.class,
+    PredefHorizontalWhiteSpaceTest.class,
+    PredefNonHorizontalWhiteSpaceTest.class,
+    PredefWhiteSpaceTest.class,
+    PredefNonWhiteSpaceTest.class,
+    PredefVerticalWhiteSpaceTest.class,
+    PredefNonVerticalWhiteSpaceTest.class,
+    PredefWordCharacterTest.class,
+    PredefNonWordCharacterTest.class
+})
+public class RunningPredefinedCharacterClassTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(RunningPredefinedCharacterClassTest.class);
+
+    /**  */
+    public static final class PredefAnyCharacterTest extends TestBase {
+        @BeforeClass
+        public static void doBeforeClass() {
+            LOGGER.debug("EXP_ANY_CHARACTER: pattern=\"{}\"", EXP_ANY_CHARACTER);
+        }
+        @Test
+        public void caseTrue() {
+            assertTrue(target.findAnyCharacter(""));
+            fail();
+        }
+        @Test
+        public void caseFalse() {
+            assertFalse(target.findAnyCharacter(""));
+            fail();
+        }
+    }
+
+    /**  */
+    public static final class PredefDigitTest extends TestBase {
+        @BeforeClass
+        public static void doBeforeClass() {
+            LOGGER.debug("EXP_A_DIGIT: pattern=\"{}\"", EXP_A_DIGIT);
+        }
+        @Test
+        public void caseTrue() {
+            assertTrue(target.findDigit(""));
+            fail();
+        }
+        @Test
+        public void caseFalse() {
+            assertFalse(target.findDigit(""));
+            fail();
+        }
+    }
+
+    /**  */
+    public static final class PredefNonDigitTest extends TestBase {
+        @BeforeClass
+        public static void doBeforeClass() {
+            LOGGER.debug("EXP_A_NON_DIGIT: pattern=\"{}\"", EXP_A_NON_DIGIT);
+        }
+        @Test
+        public void caseTrue() {
+            assertTrue(target.findNonDigit(""));
+            fail();
+        }
+        @Test
+        public void caseFalse() {
+            assertFalse(target.findNonDigit(""));
+            fail();
+        }
+    }
+
+    /**  */
+    public static final class PredefHorizontalWhiteSpaceTest extends TestBase {
+        @BeforeClass
+        public static void doBeforeClass() {
+            LOGGER.debug("EXP_HORIZONTAL_WHITE_SPACE: pattern=\"{}\"", EXP_HORIZONTAL_WHITE_SPACE);
+        }
+        @Test
+        public void caseTrue() {
+            assertTrue(target.findHorizontalWhiteSpace(""));
+            fail();
+        }
+        @Test
+        public void caseFalse() {
+            assertFalse(target.findHorizontalWhiteSpace(""));
+            fail();
+        }
+    }
+
+    /**  */
+    public static final class PredefNonHorizontalWhiteSpaceTest extends TestBase {
+        @BeforeClass
+        public static void doBeforeClass() {
+            LOGGER.debug("EXP_NON_HORIZONTAL_WHITE_SPACE: pattern=\"{}\"", EXP_NON_HORIZONTAL_WHITE_SPACE);
+        }
+        @Test
+        public void caseTrue() {
+            assertTrue(target.findNonHorizontalWhiteSpace(""));
+            fail();
+        }
+        @Test
+        public void caseFalse() {
+            assertFalse(target.findNonHorizontalWhiteSpace(""));
+            fail();
+        }
+    }
+
+    /**  */
+    public static final class PredefWhiteSpaceTest extends TestBase {
+        @BeforeClass
+        public static void doBeforeClass() {
+            LOGGER.debug("EXP_WHITE_SPACE: pattern=\"{}\"", EXP_WHITE_SPACE);
+        }
+        @Test
+        public void caseTrue() {
+            assertTrue(target.findWhiteSpace(""));
+            fail();
+        }
+        @Test
+        public void caseFalse() {
+            assertFalse(target.findWhiteSpace(""));
+            fail();
+        }
+    }
+
+    /**  */
+    public static final class PredefNonWhiteSpaceTest extends TestBase {
+        @BeforeClass
+        public static void doBeforeClass() {
+            LOGGER.debug("EXP_NON_WHITE_SPACE: pattern=\"{}\"", EXP_NON_WHITE_SPACE);
+        }
+        @Test
+        public void caseTrue() {
+            assertTrue(target.findNonWhiteSpace(""));
+            fail();
+        }
+        @Test
+        public void caseFalse() {
+            assertFalse(target.findNonWhiteSpace(""));
+            fail();
+        }
+    }
+
+    /**  */
+    public static final class PredefVerticalWhiteSpaceTest extends TestBase {
+        @BeforeClass
+        public static void doBeforeClass() {
+            LOGGER.debug("EXP_VERTICAL_WHITE_SPACE: pattern=\"{}\"", EXP_VERTICAL_WHITE_SPACE);
+        }
+        @Test
+        public void caseTrue() {
+            assertTrue(target.findVerticalWhiteSpace(""));
+            fail();
+        }
+        @Test
+        public void caseFalse() {
+            assertFalse(target.findVerticalWhiteSpace(""));
+            fail();
+        }
+    }
+
+    /**  */
+    public static final class PredefNonVerticalWhiteSpaceTest extends TestBase {
+        @BeforeClass
+        public static void doBeforeClass() {
+            LOGGER.debug("EXP_NON_VERTICAL_WHITE_SPACE: pattern=\"{}\"", EXP_NON_VERTICAL_WHITE_SPACE);
+        }
+        @Test
+        public void caseTrue() {
+            assertTrue(target.findNonVerticalWhiteSpace(""));
+            fail();
+        }
+        @Test
+        public void caseFalse() {
+            assertFalse(target.findNonVerticalWhiteSpace(""));
+            fail();
+        }
+    }
+
+    /**  */
+    public static final class PredefWordCharacterTest extends TestBase {
+        @BeforeClass
+        public static void doBeforeClass() {
+            LOGGER.debug("EXP_A_WORD_CHARACTER: pattern=\"{}\"", EXP_A_WORD_CHARACTER);
+        }
+        @Test
+        public void caseTrue() {
+            assertTrue(target.findWordCharacter(""));
+            fail();
+        }
+        @Test
+        public void caseFalse() {
+            assertFalse(target.findWordCharacter(""));
+            fail();
+        }
+    }
+
+    /**  */
+    public static final class PredefNonWordCharacterTest extends TestBase {
+        @BeforeClass
+        public static void doBeforeClass() {
+            LOGGER.debug("EXP_A_NON_WORD_CHARACTER: pattern=\"{}\"", EXP_A_NON_WORD_CHARACTER);
+        }
+        @Test
+        public void caseTrue() {
+            assertTrue(target.findAnyCharacter(""));
+            fail();
+        }
+        @Test
+        public void caseFalse() {
+            assertFalse(target.findAnyCharacter(""));
+            fail();
+        }
+    }
+
+    //--------------------------------------------------------------------------------------------------
+
+    /** */
+    public static abstract class TestBase {
+        protected PredefinedCharacterClasses target;
+        @Before
+        public void doBefore() {
+            target = new PredefinedCharacterClasses();
+        }
+    }
+}
