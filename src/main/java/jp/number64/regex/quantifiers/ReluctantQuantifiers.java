bfead58e2af1949da8d984428a4443020df7df3d
TODOROKI Daisuke <number64tech@gmail.com>
2016-01-16 14:20:30 +0900

    Implement unittests

--- src/main/java/jp/number64/regex/quantifiers/ReluctantQuantifiers.java
+++ src/main/java/jp/number64/regex/quantifiers/ReluctantQuantifiers.java
@@ -1,5 +1,85 @@
 package jp.number64.regex.quantifiers;
 
-public class ReluctantQuantifiers {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Reluctant quantifiers <br>
+ * <br>
+ * X?? X, once or not at all <br>
+ * X*? X, zero or more times <br>
+ * X+? X, one or more times <br>
+ * X{n}? X, exactly n times <br>
+ * X{n,}? X, at least n times <br>
+ * X{n,m}? X, at least n but not more than m times <br>
+ * <br>
+ */
+public class ReluctantQuantifiers {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ReluctantQuantifiers.class);
+
+    /** X?? X, once or not at all */
+    public static final String EXP_RELUC_QUANT_ONCE_OR_NOTATALL = "[a-z]??[a-z]??[a-z]??[a-z]{3}";
+    /** X*? X, zero or more times */
+    public static final String EXP_RELUC_QUANT_ZERO_OR_MORE = "[A-Z]*?[0-9][0-9]";
+    /** X+? X, one or more times */
+    public static final String EXP_RELUC_QUANT_ONE_OR_MORE = "[A-Z]+?[A-Z][A-Z]";
+    /** X{n}? X, exactly n times */
+    public static final String EXP_RELUC_QUANT_EXACTLY_N = "[A-Z]{6}?64";
+    /** X{n,}? X, at least n times */
+    public static final String EXP_RELUC_QUANT_AT_LEAST_N = "NU[A-Z]{3,}?64";
+    /** X{n,m}? X, at least n but not more than m times */
+    public static final String EXP_RELUC_QUANT_FROM_1_TO_3 = "NU[A-Z]{1,3}?64";
+    /** X{n,m}? X, at least n but not more than m times */
+    public static final String EXP_RELUC_QUANT_FROM_0_TO_2 = "NU[A-Z]{0,2}?64";
+
+    // -----------------------------------------------------------------------------------------------
+
+    private static final Pattern PATTERN_RELUC_QUANT_ONCE_OR_NOTATALL = Pattern.compile(EXP_RELUC_QUANT_ONCE_OR_NOTATALL);
+    private static final Pattern PATTERN_RELUC_QUANT_ZERO_OR_MORE = Pattern.compile(EXP_RELUC_QUANT_ZERO_OR_MORE);
+    private static final Pattern PATTERN_RELUC_QUANT_ONE_OR_MORE = Pattern.compile(EXP_RELUC_QUANT_ONE_OR_MORE);
+    private static final Pattern PATTERN_RELUC_QUANT_EXACTLY_N = Pattern.compile(EXP_RELUC_QUANT_EXACTLY_N);
+    private static final Pattern PATTERN_RELUC_QUANT_AT_LEAST_N = Pattern.compile(EXP_RELUC_QUANT_AT_LEAST_N);
+    private static final Pattern PATTERN_RELUC_QUANT_FROM_1_TO_3 = Pattern.compile(EXP_RELUC_QUANT_FROM_1_TO_3);
+    private static final Pattern PATTERN_RELUC_QUANT_FROM_0_TO_2 = Pattern.compile(EXP_RELUC_QUANT_FROM_0_TO_2);
+
+    private boolean findAndExtract(String extractTarget, Matcher sample) {
+        boolean findResult = sample.find();
+        if (findResult) {
+            LOGGER.debug("true :{} from {}", extractTarget.substring(sample.start(), sample.end()), extractTarget);
+        } else {
+            LOGGER.debug("false:{}", extractTarget);
+        }
+        return findResult;
+    }
+
+    public boolean findRelucQuantOnceOrNotAtAll(String text) {
+        return findAndExtract(text, PATTERN_RELUC_QUANT_ONCE_OR_NOTATALL.matcher(text));
+    }
+
+    public boolean findRelucQuantZeroOrMore(String text) {
+        return findAndExtract(text, PATTERN_RELUC_QUANT_ZERO_OR_MORE.matcher(text));
+    }
+
+    public boolean findRelucQuantOneOrMore(String text) {
+        return findAndExtract(text, PATTERN_RELUC_QUANT_ONE_OR_MORE.matcher(text));
+    }
+
+    public boolean findRelucQuantExactlyN(String text) {
+        return findAndExtract(text, PATTERN_RELUC_QUANT_EXACTLY_N.matcher(text));
+    }
+
+    public boolean findRelucQuantAtLeastN(String text) {
+        return findAndExtract(text, PATTERN_RELUC_QUANT_AT_LEAST_N.matcher(text));
+    }
+
+    public boolean findRelucQuantFrom1to3(String text) {
+        return findAndExtract(text, PATTERN_RELUC_QUANT_FROM_1_TO_3.matcher(text));
+    }
+
+    public boolean findRelucQuantFrom0to2(String text) {
+        return findAndExtract(text, PATTERN_RELUC_QUANT_FROM_0_TO_2.matcher(text));
+    }
 }
--- src/test/java/jp/number64/regex/quantifiers/RunningReluctantQuantifiersTest.java
+++ src/test/java/jp/number64/regex/quantifiers/RunningReluctantQuantifiersTest.java
@@ -1,5 +1,168 @@
 package jp.number64.regex.quantifiers;
 
-public class RunningReluctantQuantifiersTest {
+import static jp.number64.regex.quantifiers.ReluctantQuantifiers.*;
+import static org.junit.Assert.*;
+import jp.number64.regex.quantifiers.RunningReluctantQuantifiersTest.RelucQuant0to2TimesTest;
+import jp.number64.regex.quantifiers.RunningReluctantQuantifiersTest.RelucQuant1to3TimesTest;
+import jp.number64.regex.quantifiers.RunningReluctantQuantifiersTest.RelucQuantAtLeastNTimesTest;
+import jp.number64.regex.quantifiers.RunningReluctantQuantifiersTest.RelucQuantExactlyNTimesTest;
+import jp.number64.regex.quantifiers.RunningReluctantQuantifiersTest.RelucQuantOnceOrNotAtAllTest;
+import jp.number64.regex.quantifiers.RunningReluctantQuantifiersTest.RelucQuantOneOrMoreTest;
+import jp.number64.regex.quantifiers.RunningReluctantQuantifiersTest.RelucQuantZeroOrMoreTest;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Reluctant quantifiers test
+ */
+@RunWith(Suite.class)
+@SuiteClasses({
+    RelucQuantOnceOrNotAtAllTest.class,
+    RelucQuantZeroOrMoreTest.class,
+    RelucQuantOneOrMoreTest.class,
+    RelucQuantExactlyNTimesTest.class,
+    RelucQuantAtLeastNTimesTest.class,
+    RelucQuant1to3TimesTest.class,
+    RelucQuant0to2TimesTest.class
+})
+public class RunningReluctantQuantifiersTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(RunningGreedyQuantifiersTest.class);
+
+    /**  */
+    public static final class RelucQuantOnceOrNotAtAllTest extends TestBase {
+        @BeforeClass
+        public static void doBeforeClass() {
+            LOGGER.debug("EXP_RELUC_QUANT_ONCE_OR_NOTATALL: pattern=\"{}\"", EXP_RELUC_QUANT_ONCE_OR_NOTATALL);
+        }
+        @Test
+        public void caseTrue() {
+            assertTrue(target.findRelucQuantOnceOrNotAtAll("abc"));
+            assertTrue(target.findRelucQuantOnceOrNotAtAll("abcde"));
+            assertTrue(target.findRelucQuantOnceOrNotAtAll("abcdefgh")); // see result carefully.
+        }
+        @Test
+        public void caseFalse() {
+            assertFalse(target.findRelucQuantOnceOrNotAtAll("ab"));
+        }
+    }
+
+    /** NUMBER64 into [A-Z]*?[0-9][0-9] != (null)64, N->U->M->B->E->R->64 match! */
+    public static final class RelucQuantZeroOrMoreTest extends TestBase {
+        @BeforeClass
+        public static void doBeforeClass() {
+            LOGGER.debug("EXP_RELUC_QUANT_ZERO_OR_MORE: pattern=\"{}\"", EXP_RELUC_QUANT_ZERO_OR_MORE);
+        }
+        @Test
+        public void caseTrue() {
+            assertTrue(target.findRelucQuantZeroOrMore("NUMBER64"));
+            assertTrue(target.findRelucQuantZeroOrMore("64"));
+            assertTrue(target.findRelucQuantZeroOrMore("123"));
+        }
+        @Test
+        public void caseFalse() {
+            assertFalse(target.findRelucQuantZeroOrMore("1"));
+        }
+    }
+
+    /**  */
+    public static final class RelucQuantOneOrMoreTest extends TestBase {
+        @BeforeClass
+        public static void doBeforeClass() {
+            LOGGER.debug("EXP_RELUC_QUANT_ONE_OR_MORE: pattern=\"{}\"", EXP_RELUC_QUANT_ONE_OR_MORE);
+        }
+        @Test
+        public void caseTrue() {
+            assertTrue(target.findRelucQuantOneOrMore("NUMBER"));
+            assertTrue(target.findRelucQuantOneOrMore("NO64TODOROKI"));
+        }
+        @Test
+        public void caseFalse() {
+            assertFalse(target.findRelucQuantOneOrMore("N"));
+        }
+    }
+
+    /**  */
+    public static final class RelucQuantExactlyNTimesTest extends TestBase {
+        @BeforeClass
+        public static void doBeforeClass() {
+            LOGGER.debug("EXP_RELUC_QUANT_EXACTLY_N: pattern=\"{}\"", EXP_RELUC_QUANT_EXACTLY_N);
+        }
+        @Test
+        public void caseTrue() {
+            assertTrue(target.findRelucQuantExactlyN("NUMBER64"));
+        }
+        @Test
+        public void caseFalse() {
+            assertFalse(target.findRelucQuantExactlyN("NUmBER64"));
+        }
+    }
+
+    /**  */
+    public static final class RelucQuantAtLeastNTimesTest extends TestBase {
+        @BeforeClass
+        public static void doBeforeClass() {
+            LOGGER.debug("EXP_RELUC_QUANT_AT_LEAST_N: pattern=\"{}\"", EXP_RELUC_QUANT_AT_LEAST_N);
+        }
+        @Test
+        public void caseTrue() {
+            assertTrue(target.findRelucQuantAtLeastN("NUMBE64"));
+            assertTrue(target.findRelucQuantAtLeastN("NUMBERRRRRRRRRRRRR64"));
+        }
+        @Test
+        public void caseFalse() {
+            assertFalse(target.findRelucQuantAtLeastN("NUMB64"));
+        }
+    }
+
+    /**  */
+    public static final class RelucQuant1to3TimesTest extends TestBase {
+        @BeforeClass
+        public static void doBeforeClass() {
+            LOGGER.debug("EXP_RELUC_QUANT_FROM_1_TO_3: pattern=\"{}\"", EXP_RELUC_QUANT_FROM_1_TO_3);
+        }
+        @Test
+        public void caseTrue() {
+            assertTrue(target.findRelucQuantFrom1to3("NUM64"));
+            assertTrue(target.findRelucQuantFrom1to3("NUMBE64"));
+        }
+        @Test
+        public void caseFalse() {
+            assertFalse(target.findRelucQuantFrom1to3("NUMBER64"));
+        }
+    }
+
+    /**  */
+    public static final class RelucQuant0to2TimesTest extends TestBase {
+        @BeforeClass
+        public static void doBeforeClass() {
+            LOGGER.debug("EXP_RELUC_QUANT_FROM_0_TO_2: pattern=\"{}\"", EXP_RELUC_QUANT_FROM_0_TO_2);
+        }
+        @Test
+        public void caseTrue() {
+            assertTrue(target.findRelucQuantFrom0to2("NU64"));
+            assertTrue(target.findRelucQuantFrom0to2("NUMB64"));
+        }
+        @Test
+        public void caseFalse() {
+            assertFalse(target.findRelucQuantFrom0to2("NUMBE64"));
+        }
+    }
+
+    //--------------------------------------------------------------------------------------------------
+
+    /** */
+    public static abstract class TestBase {
+        protected ReluctantQuantifiers target;
+        @Before
+        public void doBefore() {
+            target = new ReluctantQuantifiers();
+        }
+    }
 }
